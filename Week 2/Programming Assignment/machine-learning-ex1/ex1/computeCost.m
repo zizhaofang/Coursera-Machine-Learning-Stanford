cb6e5cf0cf7d3819d4185445e4585bf69ff7a16a
zizhaofang <36433693+zizhaofang@users.noreply.github.com>
2020-01-12 22:49:11 -0800

    Update computeCost.m

--- Week 2/Programming Assignment/machine-learning-ex1/ex1/computeCost.m
+++ Week 2/Programming Assignment/machine-learning-ex1/ex1/computeCost.m
@@ -5,7 +5,6 @@ function J = computeCost(X, y, theta)
 
 m = length(y);
 
-i = 1:m;
-J = (1/(2*m)) * sum( ((theta(1) + theta(2) .* X(i,2)) - y(i)) .^ 2); % Un-Vectorized
+J = sum((X * theta - y).^2) / (2*m) 
 
 end
